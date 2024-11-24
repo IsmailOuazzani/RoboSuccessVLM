db083fb04b9bb9c3d3595c79f69bf642cdf78822
ismail <ismail.ouazzani@mail.utoronto.ca>
2024-11-24 16:45:20 -0500

    split episodes into subsequences of programmable length and stride

--- pre-processing.py
+++ pre-processing.py
@@ -1,8 +1,12 @@
 import logging
+import os
+from hashlib import sha256
 from pathlib import Path
 
 import tensorflow as tf
 import tensorflow_datasets as tfds
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -10,24 +14,36 @@ logger.setLevel(logging.INFO)
 RAW_DATASET_DIR = "/media/ismail/WDC"
 PROCESSED_DATASET_DIR = "/media/ismail/BIG/datasets/droid-processed"
 
-SPLIT_SIZE = 500
+SPLIT_SIZE = 20
 LAST_STEP_SHIFT = 5
 
 
-def process_episode(episode: tf.data.Dataset, output_path: Path):
-    first_step = tf.data.experimental.get_single_element(episode["steps"].take(1))
-    middle_step = tf.data.experimental.get_single_element(
-        episode["steps"].skip(len(episode["steps"]) // 2).take(1)
-    )
-    last_step = tf.data.experimental.get_single_element(
-        episode["steps"].skip(len(episode["steps"]) - LAST_STEP_SHIFT).take(1)
-    )
-    first_image = first_step["observation"]["wrist_image_left"]
-    middle_image = middle_step["observation"]["wrist_image_left"]
-    last_image = last_step["observation"]["wrist_image_left"]
-    image = tf.concat([first_image, middle_image, last_image], axis=1)
-    image = tf.image.encode_png(image)
-    image = tf.io.write_file(output_path.as_posix(), image)
+def process_episode(
+    episode: tf.data.Dataset,
+    output_path: Path,
+    language_instruction: str,
+    num_subsequences: int = 4,
+    steps_per_subsequence: int = 3,
+):
+    steps = list(episode["steps"])
+    instruction_hash = sha256((f"{language_instruction}").encode()).hexdigest()[0:10]
+    subsequence_size = len(steps) // num_subsequences
+
+    for i in range(num_subsequences):
+        start_idx = i * subsequence_size
+
+        end_idx = start_idx + subsequence_size
+        full_subsequence = steps[start_idx:end_idx]
+        subsequence = []
+        for j in range(
+            0, len(full_subsequence), len(full_subsequence) // steps_per_subsequence
+        ):
+            subsequence.append(full_subsequence[j])
+
+        images = [step["observation"]["wrist_image_left"] for step in subsequence]
+        image = tf.concat(images, axis=1)
+        file_path = output_path / f"{instruction_hash}_{i}.png"
+        tf.io.write_file(file_path.as_posix(), tf.image.encode_png(image))
 
 
 if __name__ == "__main__":
@@ -53,4 +69,8 @@ if __name__ == "__main__":
         logging.info(f"Instruction: {language_instruction}")
         if language_instruction:
             # TODO: check for success
-            process_episode(episode=episode, output_path=Path("test.png"))
+            process_episode(
+                episode=episode,
+                output_path=Path("./temp/"),
+                language_instruction=language_instruction,
+            )
